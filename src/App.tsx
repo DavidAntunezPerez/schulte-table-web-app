fbbec49cccdaf784bd94c615150a3acbd916199c
antunezdavid2003 <antunezdavid2003@gmail.com>
2025-08-28 10:20:10 +0000

    Installed HeroUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
+import {Button} from "@heroui/react";
 
 function App() {
 
   return (
-    <div className='flex items-center justify-center w-screen h-screen'>
+    <div className='flex flex-col items-center justify-center w-screen h-screen'>
       <p className="p-2 font-bold">
        Hello, World
       </p>
+      <Button color="primary">Button</Button>
     </div>
   )
 }
