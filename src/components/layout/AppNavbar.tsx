235a7ec3d34ba54acb9b286ed3bbdfe7b8e467e0
antunezdavid2003 <antunezdavid2003@gmail.com>
2025-09-01 11:49:01 +0000

    Added app logo

--- src/components/layout/AppNavbar.tsx
+++ src/components/layout/AppNavbar.tsx
@@ -1,9 +1,11 @@
 import { Navbar, NavbarBrand } from '@heroui/react'
+import { AppLogo } from '../../utils/logos.utils'
 
 export default function AppNavbar() {
 	return (
 		<Navbar maxWidth="full" className="bg-[#242424]">
 			<NavbarBrand>
+				<AppLogo width="48px" height="48px" />
 				<p className="font-bold text-inherit">SCHULTE TABLE</p>
 			</NavbarBrand>
 		</Navbar>
