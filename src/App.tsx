5f5a5901e067d88e61bc2bf0ef798647e8df5d66
antunezdavid2003 <antunezdavid2003@gmail.com>
2025-09-01 12:13:32 +0000

    Added current number and limit number

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
-import AppContent from './components/layout/AppContent'
+import AppContent from './components/layout/content/AppContent'
 import AppNavbar from './components/layout/AppNavbar'
 
 function App() {
 	return (
 		<div className="flex flex-col items-center w-screen h-screen">
 			<AppNavbar />
-			<div className="flex items-center justify-center w-full h-full">
 				<AppContent />
-			</div>
 		</div>
 	)
 }
