6e058e54d53c9a38e0b723a8244c71f2cb4e90de
antunezdavid2003 <antunezdavid2003@gmail.com>
2025-09-11 09:04:04 +0000

    Added error and correct sounds

--- src/utils/content.utils.ts
+++ src/utils/content.utils.ts
@@ -1,3 +1,4 @@
+// Handle table rounded corners css
 export function getCellClasses(index: number, gridSize: number) {
 	// Corners
 	if (index === 0) return 'rounded-tl-[1.6rem]' // top-left corner
@@ -9,6 +10,7 @@ export function getCellClasses(index: number, gridSize: number) {
 	return ''
 }
 
+// Math shuffle values in table
 export function shuffle<T>(array: T[]): T[] {
 	const result = [...array]
 	for (let i = result.length - 1; i > 0; i--) {
@@ -18,8 +20,24 @@ export function shuffle<T>(array: T[]): T[] {
 	return result
 }
 
+// Format time for finalTimer
 export function formatTime(ms: number): string {
 	const seconds = Math.floor(ms / 1000)
 	const milliseconds = ms % 1000
 	return `${seconds}.${milliseconds.toString().padStart(3, '0')}s`
+}
+
+// Manage audio sounds onclick
+const correctAudio = new Audio('/sounds/pop.mp3')
+const errorAudio = new Audio('/sounds/error.mp3')
+
+export function playSound(type: 'correct' | 'error') {
+	try {
+		const audio = type === 'correct' ? correctAudio : errorAudio
+		audio.currentTime = 0
+		audio.volume = type === 'correct' ? 1 : 0.35
+		audio.play()
+	} catch (e) {
+		console.warn('Error reproducing sound:', e)
+	}
 }
